b34d7234982a79f3a7d1e409d25a0bf9a3df6578
mattiamisfer <mattiamisfer@gmail.com>
2020-01-29 14:46:05 +0530

    Updates

---
+++ precache-manifest.e35f7b31664024b18afd63e3021feae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6651aa622faf048ffcdab7a1a0b4e991",
+    "url": "/tenants/index.html"
+  },
+  {
+    "revision": "dc3ccebb3ecebdc43d2d",
+    "url": "/tenants/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/tenants/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tenants/static/js/2.b6fabe4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc3ccebb3ecebdc43d2d",
+    "url": "/tenants/static/js/main.ebccb30f.chunk.js"
+  },
+  {
+    "revision": "49b04b57f1f5c3ea3bac",
+    "url": "/tenants/static/js/runtime-main.af296e4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tenants/static/media/logo.5d5d9eef.svg"
+  }
+]);
